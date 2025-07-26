121c108c4f6b963a9efde0afc3ec027c35e1b7d4
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-07-26 08:40:20 +0800

    refactor: improve code formatting and consistency in GET function

--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -4,25 +4,24 @@ import jwt from 'jsonwebtoken';
 const SECRET = 'your-secret-key';
 
 interface JwtPayload {
-    username: string;
-    iat: number;
-    exp: number;
+  username: string;
+  iat: number;
+  exp: number;
 }
 
-export default function GET(req: NextRequest) {
-    const authHeader = req.headers.get('authorization');
+export async function GET(req: NextRequest) {
+  const authHeader = req.headers.get('authorization');
 
-    if(!authHeader) {
-        return NextResponse.json({message: 'No token Provided'}, {status: 401});
-    }
+  if (!authHeader) {
+    return NextResponse.json({ message: 'No token Provided' }, { status: 401 });
+  }
 
-    const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1];
 
-    try {
-        const decoded = jwt.verify(token, SECRET) as JwtPayload;
-
-        return NextResponse.json({ message: `Hello, ${decoded.username}`}, {status: 200})
-    } catch (err) {
-        return NextResponse.json({ message: 'Invalid or Expired Token'}, {status: 403});
-    }
+  try {
+    const decoded = jwt.verify(token, SECRET) as JwtPayload;
+    return NextResponse.json({ message: `Hello, ${decoded.username}` }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: 'Invalid or Expired Token' }, { status: 403 });
+  }
 }
